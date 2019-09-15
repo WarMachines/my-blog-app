2b4a8d249e777cb6e2cade2345117efe0475cbc4
Saurabh <saurabhchauhan9194@gmail.com>
2019-09-15 18:03:51 -0400

    first commit

---
+++ src/build/precache-manifest.9cc499efb4c5919779613f099c131db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb45af49ae13baaa065efb21f5439c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afaf0ee9f5d7b154cb97",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "09e87e347a8f27938e57",
+    "url": "/static/js/2.88c4fb3f.chunk.js"
+  },
+  {
+    "revision": "afaf0ee9f5d7b154cb97",
+    "url": "/static/js/main.1b870c21.chunk.js"
+  },
+  {
+    "revision": "45daf7fcf4c8a047681b",
+    "url": "/static/js/runtime~main.cf726479.js"
+  }
+]);
